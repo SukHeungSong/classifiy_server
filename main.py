906711e3b5bc7dff4af52f22355e3b1b59bf2d89
Lightsoo <kks3805han@gmail.com>
2016-08-12 17:32:09 +0900

    flask구축

--- main.py
+++ main.py
@@ -11,7 +11,34 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.svm import LinearSVC
 from sklearn.grid_search import GridSearchCV
 import numpy as np
+# 만들어진 모델을 나중에 분류할때 사용하기 위해..
+from sklearn.externals import joblib
 
+from flask import request
+
+import flask
+import optparse
+
+
+# Obtain the flask app object
+app = flask.Flask(__name__)
+
+@app.route('/')
+def index():
+    return flask.render_template('index.html', has_result=False)
+
+@app.route('/classifier', methods=['GET'])
+def classifier():
+    # name = flask.request.args.get('name', '')
+
+    name = request.args.get('name')
+    print(name)
+
+
+    # logging.info('Image: %s', name)
+    # result = app.clf.classify_image(image)
+
+    return flask.jsonify(name)
 
 
 def test() :
@@ -79,8 +106,67 @@ def test() :
     svc_param = {'C': np.logspace(-2, 0, 20)}
     gs_svc = GridSearchCV(LinearSVC(loss='l2'), svc_param, cv=5, n_jobs=4)
     gs_svc.fit(x_list, y_list)
-    print(gs_svc.best_params_, gs_svc.best_score_)
+    # print(gs_svc.best_params_, gs_svc.best_score_)
+
+
+    clf = LinearSVC(C=gs_svc.best_params_['C'])
+    clf.fit(x_list, y_list)
+
+    # joblib.dump(clf, 'classify.model', compress=3)
+    # joblib.dump(cate_dict, 'cate_dict.dat', compress=3)
+    # joblib.dump(vectorizer, 'vectorizer.dat', compress=3)
 
     return
 
-test()
+
+
+
+def flaskrun(app, default_host="127.0.0.1",
+                  default_port="3000"):
+    """
+    Takes a flask.Flask instance and runs it. Parses
+    command-line flags to configure the app.
+    """
+
+    # Set up the command-line options
+    parser = optparse.OptionParser()
+    parser.add_option("-H", "--host",
+                      help="Hostname of the Flask app " + \
+                           "[default %s]" % default_host,
+                      default=default_host)
+    parser.add_option("-P", "--port",
+                      help="Port for the Flask app " + \
+                           "[default %s]" % default_port,
+                      default=default_port)
+
+    # Two options useful for debugging purposes, but
+    # a bit dangerous so not exposed in the help message.
+    parser.add_option("-d", "--debug",
+                      action="store_true", dest="debug",
+                      help=optparse.SUPPRESS_HELP)
+    parser.add_option("-p", "--profile",
+                      action="store_true", dest="profile",
+                      help=optparse.SUPPRESS_HELP)
+
+    options, args = parser.parse_args()
+
+    # If the user selects the profiling option, then we need
+    # to do a little extra setup
+    if options.profile:
+        from werkzeug.contrib.profiler import ProfilerMiddleware
+
+        app.config['PROFILE'] = True
+        app.wsgi_app = ProfilerMiddleware(app.wsgi_app,
+                       restrictions=[30])
+        options.debug = True
+
+    app.run(
+        debug=options.debug,
+        host=options.host,
+        port=int(options.port)
+    )
+
+
+# test()
+if __name__ == '__main__':
+    flaskrun(app)
