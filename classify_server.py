a105014027f2d3f88d78d14fb0182525aae8cad6
Lightsoo <kks3805han@gmail.com>
2016-08-23 08:08:06 +0900

    d_list(상품명리스트)에서 상품이름을 하나씩 형태소 분석하고 이를 통해 의미있는 단어를 추출하고 새로운 리스트를 만들어서 분류성능평가를 하려고하였다 하지만...추출하면서 메모리리크 발생

---
+++ classify_server.py
@@ -0,0 +1,34 @@
+from sklearn.externals import joblib
+from bottle import route, run, template,request,get, post
+
+
+import  time
+
+
+clf = joblib.load('classify.model')
+cate_dict = joblib.load('cate_dict.dat')
+vectorizer = joblib.load('vectorizer.dat')
+
+joblib.dump(clf,'n_classify.model')
+
+joblib.dump(cate_dict,'n_cate_dict.dat')
+joblib.dump(vectorizer,'n_vectorizer.dat')
+
+cate_id_name_dict = dict([(v, k) for k, v in cate_dict.items()])
+
+pred = clf.predict(vectorizer.transform(['[신한카드5%할인][서우한복] 아동한복 여자아동 금나래 (분홍)']))[0]
+print (cate_id_name_dict[pred])
+
+
+from threading import  Condition
+_CONDITION = Condition()
+@route('/classify')
+def classify():
+    print ("classify called")
+    img = request.GET.get('img','')
+    name = request.GET.get('name', '')
+    pred = clf.predict(vectorizer.transform([name]))[0]
+    return {'cate':cate_id_name_dict[pred]}
+
+
+run(host='0.0.0.0', port=8887)
--- main.py
+++ main.py
@@ -1,28 +1,21 @@
 
 # feture를 기존 저장되어 있는 data와 비교하여 가장 유사도가 높은 ID를 찾아내서 분류한다
-#
-#
 
 # stop word removal
 import nltk
+from nltk.tokenize import word_tokenize
+from konlpy.tag import Twitter
 
-from konlpy.tag import Kkma
-from konlpy.utils import pprint
-
-from konlpy.tag import Kkma
-from konlpy.corpus import kolaw
-# from konlpy.utils import pprint
 from nltk import collocations
-from nltk.corpus import stopwords
+from nltk.tag import pos_tag
 
+from nltk.corpus import gutenberg
+from nltk import regexp_tokenize
 
 
 #한글 형태소 분석기
 from konlpy.tag import Twitter
 pos_tagger = Twitter()
-
-def tokenizer(doc):
-    return['/'.join(text)for text in pos_tagger(doc, norm = True)]
 
 
 import pandas as pd
@@ -74,114 +67,70 @@ def classifier():
     # return flask.jsonify(name)
 
 
-
 def setModel() :
+    measures = collocations.BigramAssocMeasures()
+    twitter = Twitter()
 
-    path = "./soma_classifier.csv"
-    train_df = pd.read_csv(path)
+    # path = "./soma_classifier.csv"
+    # train_df = pd.read_csv(path)
+    train_df = pd.read_pickle("soma_goods_train.df")
 
-    # train_df = pd.read_pickle("soma_goods_train.df")
+    # nltk.download("stopwords")
+    # nltk.download("punkt")
+    # nltk.download("maxent_treebank_pos_tagger")
+    # nltk.download()
 
+    # pattern = r'''(?x) ([A-Z]\.)+ | \w+(-\w+)* | \$?\d+(\.\d+)?%? | \.\.\. | [][.,;"'?():-_`]'''
     # 학습하는 상품 name들을 리스트로 만든다.
     d_list = []
     # 카테고리 분류명에 구분자(';')를 줘서 리스트를 만든다
     cate_list = []
-    # 학습할 리스트
-    l_list=[]
+
 
     # iterrows() : 행의 이름과 값들을 쌍으로 조회
     for each in train_df.iterrows():
-        # print(each);
         # join() : ';'를 구분자로 내용들을 합친다
         cate = ";".join([each[1]['cate1'], each[1]['cate2'], each[1]['cate3']])
         d_list.append(each[1]['name'])
         cate_list.append(cate)
-
-    # print(d_list)
-
-    # nltk.download("stopwords")
-    # stops = set(stopwords.words("english"))
+    # print(type(d_list[0]))
+    print(d_list)
+    # print(len(d_list))
+    # words =[]
+    # print('Collocations among tagged words:')
+    # for d in d_list :
+    #     words.append(' '.join(twitter.nouns(d)))
     #
-    # for d in d_list:
-    #     for w in d.split():
-    #         if w.lower() not in stops:
-    #             l_list.append(w)
+    # print(words)
+    pattern = r'''(?x) ([A-Za-z]\.)+ | \w+(-\w+)* | \$?\d+(\.\d+)?%? | \.\.\. | [][.,;"'?():-_`]'''
 
-    measures = collocations.BigramAssocMeasures()
-    kkma = Kkma()
-
-    # print(x_list)
-    # 단어들 출력
-    tagged_words = []
-    words =[]
-    temp =[]
-    print('\nCollocations among tagged words:')
+    words_kor =[]
+    words=[]
+    # print('Collocations among tagged words:')
     for d in d_list :
-        # tagged_words = kkma.pos(d)
-        # tagged_words = kkma.nouns(d)
-        print(kkma.nouns(d))
-        # tagged_words.append(kkma.nouns(d))
+        # tokens_en = regexp_tokenize(d, pattern)
+        # tokens = word_tokenize(d)
+        tokens = d.split()
+        # print(tokens)
+        # print(twitter.nouns(d))
+        # words_kor.append(' '.join(twitter.nouns(d)))
+        words.append(' '.join(tokens))
 
-        words.append(' '.join(kkma.nouns(d)))
-        # print(tagged_words)
-
-        # temp = []
-        # for word in tagged_words :
-            # print(word)
-            # temp.append(word)
-        # finder = collocations.BigramCollocationFinder.from_words(tagged_words)
-        # temp = finder.nbest(measures.pmi, 10)
-        # print(temp)
-        # print(finder.nbest(measures.pmi, 10))
-        # print(tagged_words)
-
-        # finder = collocations.BigramCollocationFinder.from_words(tagged_words)
-        # print(finder.nbest(measures.pmi, 10))  # top 5 n-grams with highest PMI
-
-        # for d in d.split() :
-            # temp = kkma.nouns(d)
-            # print(temp)
-            # temp.append(kkma.nouns(d))
-            # pos = kkma.pos(d)
-            # pos = kkma.nouns(d)
-
-            # l_list.append(pos)
-            # print(pos)
-        # print(temp)
-        # words.append(' '.join(temp))
-        # print(words)
-    # print(tagged_words)
-    # print(len(tagged_words))
+    # print(type(words[0]))
+    print(words_kor)
 
 
+    words_eng = []
+    # for d in d_list:
+        # print(d)
+        # tagged_sent = pos_tag(d.split())
+        # print(tagged_sent)
+        # nouns = [word for word, pos in tagged_sent if pos == 'Noun']
+        # print(nouns)
 
 
-    # for word in tagged_words :
-    #     # temp.append(word)
-    #     words.append(' '.join(word))
-    print(words)
-    print(len(words))
-
-
-
-
-
-
-
-
-
-
-
-
-    # words = [w for w, t in tagged_words]
-    # # ignored_words = [u'안녕']
-    # finder = collocations.BigramCollocationFinder.from_words(words)
-    # # finder.apply_word_filter(lambda w: len(w) < 2 or w in ignored_words)
-    #
-    # # bigram 중에서 3번 이상 나오는 단어가 출력된다
-    # finder.apply_freq_filter(3)
-    # temp = finder.nbest(measures.pmi, 10)
-    # print(temp)
+    # print(len(words))
+    # 여기까지!!!!!!~~~~~~~~~~~!~!~!~!~!@~!@~!@~!@!~@!@~!~!~!~!~!~!~!~!~!~!~!~!~!~!!!!!!!~~~~~~~~~~~!~!~!~!~!@~!@~!@~!@!~@!@~!~!~!~!~!~!~!~!~!~!~!~!~!~!
 
 
     # 같은종류를 묶어서 하나로...group by와 같다!!
@@ -193,6 +142,7 @@ def setModel() :
     # dict() : value : key(id); 형태로 저장이 된다,
 
     cate_dict = dict(zip(list(set(cate_list)), range(len(set(cate_list)))))
+
     # print(cate_dict), {'디지털/가전;PC부품;CPU' : O, '패션의류;아동의류;한복':1}
     # print(cate_dict['디지털/가전;PC부품;CPU']), 0
 
@@ -207,20 +157,21 @@ def setModel() :
 
     # 각 상품별로 name의 문장에 있는 단어들을 빈도수를 matrix형태로 만든다.
     vectorizer = CountVectorizer()
-    x_list = vectorizer.fit_transform(d_list)
-    # x_list = vectorizer.fit_transform(words)
+    # x_list = vectorizer.fit_transform(d_list)
+    x_list = vectorizer.fit_transform(words)
 
+    # print(x_list.shape)
     # print(x_list)
     # 단어들 출력
     # for x in x_list:
-    #     for word in x.indices :
-    #         doc = vectorizer.get_feature_names()[word]
-    #         pos = kkma.pos(doc)
-    #         # words = [s for s, t in pos]
-    #         # tags = [t for s, t in pos]
+    #     print(x.indices)
+    #     print(x.data)
+    #     print(x.indptr)
 
-    # print("끝")
-    # print(d_list)
+        # for word in x.indices :
+        #     doc = vectorizer.get_feature_names()[word]
+        #     print(doc)
+
 
     # vectorizer100 = CountVectorizer(max_features=100)
     # x100_list = vectorizer100.fit_transform(d_list)
@@ -232,24 +183,22 @@ def setModel() :
     # for x10 in x100_list:
     #     for word in x10.indices :
     #         temp = kkma.pos(vectorizer100.get_feature_names()[word])
-            # print(temp)
-            # print(vectorizer100.get_feature_names()[word])
 
-    # svc_param = {'C': np.logspace(-2, 0, 20)}
-    # # svc_param = {'C': np.logspace(-2, 0, 5)}
+    svc_param = {'C': np.logspace(-2, 0, 20)}
+    # svc_param = {'C': np.logspace(-2, 0, 5)}
     #
-    # gs_svc = GridSearchCV(LinearSVC(loss='l2'), svc_param, cv=5, n_jobs=4)
-    # gs_svc.fit(x_list, y_list)
-    #
-    # print(gs_svc.best_params_, gs_svc.best_score_)
-    #
-    #
-    # clf = LinearSVC(C=gs_svc.best_params_['C'])
-    # clf.fit(x_list, y_list)
+    gs_svc = GridSearchCV(LinearSVC(loss='l2'), svc_param, cv=5, n_jobs=4)
+    gs_svc.fit(x_list, y_list)
 
-    # joblib.dump(clf, 'classify.model', compress=3)
-    # joblib.dump(cate_dict, 'cate_dict.dat', compress=3)
-    # joblib.dump(vectorizer, 'vectorizer.dat', compress=3)
+    print(gs_svc.best_params_, gs_svc.best_score_)
+
+
+    clf = LinearSVC(C=gs_svc.best_params_['C'])
+    clf.fit(x_list, y_list)
+
+    joblib.dump(clf, 'classify.model', compress=3)
+    joblib.dump(cate_dict, 'cate_dict.dat', compress=3)
+    joblib.dump(vectorizer, 'vectorizer.dat', compress=3)
     return
 
 def test():
